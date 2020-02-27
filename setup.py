12c4c1221c266cf0619f4d4e0e682123788f9640
Arian Amiramjadi <Arianaamjadi@gmail.com>
2020-02-27 18:06:23 +0330

    add entry point

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='WARP+ to WireGuard',
-      version='0.202',
+      version='0.3',
       description='A simple cli to get WARP+ as WireGuard configuration',
       url='https://git.arian.lol/warp-plus/python',
       author='Arian Amiramjadi',
       author_email='me@arian.lol',
       packages=['warpy'],
-      install_requires=['pynacl'],
+      install_requires=['pynacl', "requests"],
+      entry_points={'console_scripts': [
+            'warpy = warpy.__main__',
+        ]},
       scripts=['scripts/warpy.py'])
